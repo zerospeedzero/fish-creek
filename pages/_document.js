8f6b0aeb9e1b9d378c76e6063dd6cf431a77ccf7
zerospeedzero <zerospeedzero@gmail.com>
2023-05-21 23:10:24 -0600

    Second draft

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import Navbar from '../components/Navbar'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/> 
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
+        {/* <Navbar/> */}
         <Main />
         <NextScript />
       </body>
