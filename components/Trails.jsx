d2a9d2641d4c4535609ceaaaec58b801716c8a0c
zerospeedzero <zerospeedzero@gmail.com>
2023-05-23 20:05:36 -0600

    Enable Google Analytics

--- components/Trails.jsx
+++ components/Trails.jsx
@@ -3,7 +3,7 @@ import GoogleMap from './GoogleMap';
 
 const Trails = ({trails, message}) => {
   return (
-    <div id="trails" className1='h-screen mt-90 mb-12 bg-fixed bg-center bg-cover'>
+    <div id="trails" className='h-screen mt-90 mb-12 bg-fixed bg-center bg-cover'>
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/80 -z-10'/>
       <div className='p-5 text-white mt-[90px]'>
         <p className='text-white text-2xl w-fit font-bold mx-auto pb-[30px]'>Trails in map</p>
--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,39 @@ import '@/styles/globals.css'
 import Navbar from '../components/Navbar'
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import Script from 'next/script'
+import * as gtag from "../lib/gtag"
 
 export default function App({ Component, pageProps }) {
-  // useEffect(() => {
-  //   GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KE;
-  // }, [])
-  const { pathname } = useRouter();
+  const router = useRouter();
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
   useEffect(() => {
-    if (pathname == '/trails') {
+    const handleRouteChange = (url) => {
+        gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    if (router.pathname == '/trails') {
       window.scrollTo(0,0);
     }
-  }, [pathname]);
+    return () => {router.events.off("routeChangeComplete", handleRouteChange);};
+  }, [router]);
   return  (
     <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXX"></Script>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', GA_MEASUREMENT_ID , {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
       <Navbar/>
       <Component {...pageProps} />
       {/* <script src={"https://maps.googleapis.com/maps/api/js?key=" + process.env.NEXT_PUBLIC_GOOGLE_API_KEY }></script> */}
