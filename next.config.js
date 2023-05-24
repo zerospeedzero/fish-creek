ba316fa7ab30330493a23c13377a73ad41763c7d
zerospeedzero <zerospeedzero@gmail.com>
2023-05-24 06:34:03 -0600

    Thrid commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'images.unsplash.com', 'openweathermap.org'
+    ]
+  }
 }
 
 module.exports = nextConfig
