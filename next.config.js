f636643441f846d74036d22d76a6ed7272100162
zerospeedzero <zerospeedzero@gmail.com>
2023-06-04 08:54:38 -0600

    Disable output export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   // basePath: '/out',
   // assetPrefix: "/out",
   // basePath: '/~gcheng/fish_creek',
