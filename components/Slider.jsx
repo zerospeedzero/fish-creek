db33b08c6b322fcf8c217ea713c6d9709c1e0342
zerospeedzero <zerospeedzero@gmail.com>
2023-06-04 22:59:51 -0600

    Image from Instagram feed

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -6,17 +6,21 @@ import Carousel from 'better-react-carousel';
 import SocialMedia from './SocialMedia';
 
 const Slider = ({slides}) => {
-  const [current, setCurrent ] =useState(0);
-  const length = slides.length;
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1)
-  }
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length -1  : current - 1)
-  }
-  if (!Array.isArray(slides) || slides.length <=0 ) {
-    return null;
-  }
+  const [feeds, setFeeds ] = useState([]);
+  useEffect(() => {
+    const fetchFeeds = async () => {
+      try {
+        const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}`;
+        const data = await fetch(url);
+        const result = await data.json();
+        setFeeds(result.data);
+        console.log(result.data);
+      } catch(error) {
+        console.error('Error fetching Instagram feed image:', error);
+      }
+    }
+    fetchFeeds();
+  },[])
   const responsiveLayout = [
     { breakpoint: 9999, cols: 3, rows: 1, gap: 10, loop: true, autoplay: 10000 },
     { breakpoint: 1024, cols: 2, rows: 1, gap: 10, loop: true, autoplay: 10000 },
@@ -27,15 +31,18 @@ const Slider = ({slides}) => {
     <div id='gallery' className='mx-auto pt-[90px]'>
       <h1 className='text-2xl font-bold text-black text-center p-4'>Gallery of Fish Creek</h1>
         <Carousel gap={10} scrollSnap loop showDots responsiveLayout={responsiveLayout}>
-        {SliderData.map((slide, index) => {
+        {feeds.map((feed, index) => {
           return (
             <Carousel.Item key={index}>
-              <img 
-                src={(process.env.NODE_ENV == 'development') ? slide.image : slide.image} 
-                alt='dummy'
-                width="100%"
-                onClick={(e) => {console.log('abc1')}}
-              />
+              <h4 className='bg-black/70 -mb-1 text-center text-white'>{feed.caption == null ? "Awesome!!": feed.caption}</h4>
+              <a href={feed.permalink} target="_blank">
+                <img 
+                  src={(process.env.NODE_ENV == 'development') ? feed.media_url : feed.image} 
+                  alt='dummy'
+                  width="100%"
+                  onClick={(e) => {console.log('abc1')}}
+                />
+              </a>
               <div className="-mt-7">
                 <SocialMedia/>
               </div>
