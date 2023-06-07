5a63f998ec28cbf32cf8a405a7edd0c2a13a818c
zerospeedzero <zerospeedzero@gmail.com>
2023-06-06 23:06:01 -0600

    xs:bg-scroll is appled on HERO

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ import Spinner from '../components/Spinner';
 
 const Head = ({heading, message}) => {
   return (
-    <div id="hero" className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-image'>
+    <div id="hero" className='flex items-center justify-center h-screen mb-12 xs:bg-scroll md:bg-fixed bg-center bg-cover custom-image'>
       {/* Overlay */}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[2]'/>
       <Weather heading='Welcome to Fish Creek' message="Hiking in Calgary City"/>
