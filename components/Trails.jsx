fa388f0640c771ce9ff5a2b2ca7278d87ee96107
zerospeedzero <zerospeedzero@gmail.com>
2023-06-08 07:25:54 -0600

    Trails component is updated

--- components/Trails.jsx
+++ components/Trails.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import GoogleMap from './GoogleMap';
-import {FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon, WhatsappShareButton, WhatsappIcon, LinkedinShareButton, LinkedinIcon } from 'next-share';
-
+import SocialMedia from './SocialMedia';
 const Trails = ({trails, message}) => {
   return (
      <div id='trails' className='mx-auto pt-[90px]'>
@@ -11,38 +10,13 @@ const Trails = ({trails, message}) => {
         <GoogleMap/>
       </div>
       <div className='w-[80%] mx-auto'>
-        <h3>Trail 1 details</h3>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, quod?</p>
-        <p>Distinctio non obcaecati a quo esse? Dolorum iste eum enim!</p>
-        <p>Quaerat, nisi et? Qui eius unde eaque dolor placeat possimus!</p>
-        <p>Reiciendis quo totam, similique aliquid rerum laborum quisquam ut a!</p>
-        <div className='flex justify-center'>
-          <FacebookShareButton
-            url={'https://fish-creek.azurewebsites.net'}
-            quote={'next-share is a social share buttons for your next React apps.'}
-            hashtag={'#nextshare'}
-          >
-            <FacebookIcon size={42} round className='m-2 opacity-70 hover:opacity-100'/>
-          </FacebookShareButton>
-          <TwitterShareButton
-            url={'https://fish-creek.azurewebsites.net'}
-            title={'next-share is a social share buttons for your next React apps.'}
-          >
-            <TwitterIcon size={42} round className='m-2 opacity-70 hover:opacity-100' />
-          </TwitterShareButton>
-
-          <WhatsappShareButton
-            url={'https://fish-creek.azurewebsites.net'}
-            title={'next-share is a social share buttons for your next React apps.'}
-            separator=":: "
-          >
-            <WhatsappIcon size={42} round className='m-2 opacity-70 hover:opacity-100'/>
-          </WhatsappShareButton>
-          <LinkedinShareButton url={'https://fish-creek.azurewebsites.net'}>
-            <LinkedinIcon size={42} round className='m-2 opacity-70 hover:opacity-100'/>
-          </LinkedinShareButton>
-        </div>
+        <h3 className='text-black'>Trail 1 details</h3>
+        <p className='text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis, quod?</p>
+        <p className='text-black'>Distinctio non obcaecati a quo esse? Dolorum iste eum enim!</p>
+        <p className='text-black'>Quaerat, nisi et? Qui eius unde eaque dolor placeat possimus!</p>
+        <p className='text-black'>Reiciendis quo totam, similique aliquid rerum laborum quisquam ut a!</p>
       </div>
+      <SocialMedia/>
     </div>
   )
 }
